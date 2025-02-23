4553b34be97c977b98faf5f815bc0fe171cee4c1
Sammygarch <s@mmygarcia.com>
2025-02-23 16:24:16 +0000

    Update script.js
    Switched API to OpenWeather
    Added function to format city.

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const apiKey = 'c3275fe4fe8a25302ffdd126186f51c1';
-function getWeather(city, element) {
-    var URL = `https://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`;
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('city').innerHTML = data.location.name;
-            element.innerHTML = data.current.temperature + '°C';
-        })
-        .catch(error => console.error('Error:', error));
+const apiKey = "90647326293b1d909fb8e234621bfd11";
+async  function getCoords(city) {
+    let URL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+    try {
+        let response = await fetch(URL);
+        let data = await response.json();
+        console.log(data);
+        let latitude =  data[0].lat
+        let longitude = data[0].lon
+        let coords = [latitude, longitude];
+        return coords;
+    } catch (error) {
+        console.error('Error fetching coordinates:', error);
+        throw error;
+    }
 }
-document.getElementById('weatherForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const city = document.getElementById('search').value;
-    getWeather(city, document.getElementById('temp'));
-});
+
+function formatCity(city) {
+    // Formats the city so that the first letter is capitalised and the rest is lower case.
+    city = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
+    return city
+}
+async function getWeather(city) {
+    try {
+        let coords = await getCoords(city);
+        let URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords[0]}&lon=${coords[1]}&appid=${apiKey}&units=metric`;
+        let response = await fetch(URL);
+        let data = await response.json();
+        console.log(data)
+        return data;
+    } catch(error) {
+        console.error('Error fetching weather', error);
+        throw error;
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("submit").addEventListener("click", async function(event) {
+        event.preventDefault();
+        let city = document.getElementById("search").value;
+        document.getElementById("city").innerHTML = formatCity(city);
+        let weather = await getWeather(city);
+        let current = weather.current
+        document.getElementById("temp").innerHTML = current.temp + '°C';
+    });
+});    
